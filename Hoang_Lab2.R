7245508add885fe4a99967d58338e5cb38ef4dd9
Christine Hoang <christinehoangt@gmail.com>
2024-01-28 21:23:18 -0600

    saving lab2

---
+++ Hoang_Lab2.R
@@ -0,0 +1,16 @@
+#Christine Hoang
+
+#Create a vector named Ones that contains the numbers from 1 to 5.
+Ones<-seq(1,5)
+Ones
+#Create a vector named Twos that contains the numbers from 2 to 10 counting by 2
+Twos<-seq( to =10, from = 2, by = 2)
+Twos
+#Create three more vectors, named Threes, Fours, and Fives
+Threes<-seq(to=5, from=1, by=3)
+Threes
+Fours<-seq(to=5, from=1, by=4)
+Fours
+Fives<-seq(to=5, from=1, by=5)
+Fives
+#Create a data frame named times_table that combines your 5 vectors.
